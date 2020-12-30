d282394f9ced33487b902f6c00019af2e1b1d5f0
Boeschenstein, Patrik <patrik.boeschenstein@hotmail.com>
2020-12-30 17:35:57 +0100

    cleanup

--- backend/MySpecificTest.Infrastructure/SpecificationPattern/BaseSpecification.cs
+++ backend/MySpecificTest.Infrastructure/SpecificationPattern/BaseSpecification.cs
@@ -13,7 +13,6 @@ namespace MySpecificTest.Infrastructure.SpecificationPattern
 
         public Expression<Func<T, bool>> Criteria { get; }
         public List<Expression<Func<T, object>>> Includes { get; } = new List<Expression<Func<T, object>>>();
-        public List<string> IncludeStrings { get; } = new List<string>();
 
         protected virtual void AddInclude(Expression<Func<T, object>> includeExpression)
         {
